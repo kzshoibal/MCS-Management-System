7628c9db7c12026f3a2fffd48e9999fdaafa942f
Sagor <mnsagor@gmail.com>
2019-10-05 15:11:32 +0600

    update the app service provider

---
+++ database/migrations/2019_10_04_123507_create_notices_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoticesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('title');
+
+            $table->longText('description')->nullable();
+
+            $table->longText('contents');
+
+            $table->bigIncrements('author_id');
+
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notices');
+    }
+}
